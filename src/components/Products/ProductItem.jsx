ed88c1b5b8b145765e36535d1cbd19441ef8328e
Mehmet Kerem CANDANGIL <mkcandangil@sgk.gov.tr>
2025-08-12 11:54:59 +0300

    React state mantığı

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,22 +1,28 @@
 import "./ProductItem.css";
 import ProductInfo from "./ProductInfo";
+import {useState} from "react";
+
 const ProductItem = ({ product }) => {
   const { imageUrl, productName, productPrice } = product;
+  //let title = productName;
+  
+  const [title, setTitle] = useState(productName);
 
   const clickHandler = () => {
-    console.log("Sepete eklendi")
+    setTitle("Güncellendi");
+    console.log(productName, "Güncellendi");
   }
-  
+
   return (
     <div className="product-item">
       <div className="product-image">
         <img src={imageUrl} alt="" />
       </div>
       <ProductInfo>
-        <h2>{productName}</h2>
+        <h2>{title}</h2>
         <span>{productPrice} TL</span>
         <br />
-        <button onClick={clickHandler}>Sepete Ekle</button>
+        <button onClick={clickHandler}>Güncelle</button>
       </ProductInfo>
     </div>
   );

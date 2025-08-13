8c0e87e46a69cb1a58294a761790287b9bf3482a
Mehmet Kerem CANDANGIL <mkcandangil@sgk.gov.tr>
2025-08-13 10:10:52 +0300

    Çoklu state kullanımı

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -2,34 +2,47 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
-  const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [productData, setProductData] = useState({
+    productName: "",
+    productPrice: 0,
+    imageUrl: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setProductName(event.target.value);
+    setProductData({
+      ...productData,
+      productName: event.target.value
+    });
   }
 
   const priceChangeHandler = (event) => {
-    setProductPrice(event.target.value);
+    setProductData({
+      ...productData,
+      productPrice: event.target.value
+    })
   }
 
   const imageChangeHandler = (event) => {
-    setImageUrl(event.target.value);
+    setProductData({
+      ...productData,
+      imageUrl: event.target.value
+    })
   }
+
+  console.log(productData);
 
   return (
     <form className="product-form">
       <div className="product-form-input">
-        <label>Ürün Adı:{productName}</label>
+        <label>Ürün Adı</label>
         <input type="text" placeholder="Ürün Adı Giriniz..." onChange={titleChangeHandler}/>
       </div>
       <div className="product-form-input">
-        <label>Ürün Fiyatı:{productPrice}₺</label>
+        <label>Ürün Fiyatı</label>
         <input type="text" placeholder="Ürün Fiyatı Giriniz..." onChange={priceChangeHandler}/>
       </div> 
       <div className="product-form-input">
-        <label>Ürün Görseli:{imageUrl}</label>
+        <label>Ürün Görseli</label>
         <input type="text" placeholder="Ürün Görseli Giriniz..." onChange={imageChangeHandler}/>
       </div>
       <button className="product-form-button">Ürün Ekle</button>

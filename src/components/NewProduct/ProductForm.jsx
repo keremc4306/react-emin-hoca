524896a97479dc3b3cbdf24aeeb678f6a98d40ad
Mehmet Kerem CANDANGIL <mkcandangil@sgk.gov.tr>
2025-08-13 10:57:11 +0300

    Form onSubmit işlemi

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -2,51 +2,50 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
-  const [productData, setProductData] = useState({
-    productName: "",
-    productPrice: 0,
-    imageUrl: "",
-  });
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  const titleChangeHandler = (event) => {
-    setProductData((prevState) => {
-      return {
-        ...prevState, productName: event.target.value
-      }
-    });
+  /* const titleChangeHandler = (event) => {
+    setProductName(event.target.value);
   }
 
   const priceChangeHandler = (event) => {
-    setProductData((prevState) => {
-      return {
-        ...prevState, productPrice: event.target.value
-      }
-    });
+    setProductPrice(event.target.value);
   }
 
   const imageChangeHandler = (event) => {
-    setProductData((prevState) => {
-      return {
-        ...prevState, productPrice: event.target.value
-      }
-    });
+    setImageUrl(event.target.value);
+  } */
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newProductData = {
+      productName,
+      productPrice,
+      imageUrl,
+    };
+
+    console.log(newProductData);
+    
+    setProductName("");
+    setProductPrice("");
+    setImageUrl("");
   }
 
-  console.log(productData);
-
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={submitHandler}>
       <div className="product-form-input">
         <label>Ürün Adı</label>
-        <input type="text" placeholder="Ürün Adı Giriniz..." onChange={titleChangeHandler}/>
+        <input type="text" value={productName} placeholder="Ürün Adı Giriniz..." onChange={(event) => setProductName(event.target.value)}/>
       </div>
       <div className="product-form-input">
         <label>Ürün Fiyatı</label>
-        <input type="text" placeholder="Ürün Fiyatı Giriniz..." onChange={priceChangeHandler}/>
+        <input type="text" value={productPrice} placeholder="Ürün Fiyatı Giriniz..." onChange={(event) => setProductPrice(event.target.value)}/>
       </div> 
       <div className="product-form-input">
         <label>Ürün Görseli</label>
-        <input type="text" placeholder="Ürün Görseli Giriniz..." onChange={imageChangeHandler}/>
+        <input type="text" value={imageUrl} placeholder="Ürün Görseli Giriniz..." onChange={(event) => setImageUrl(event.target.value)}/>
       </div>
       <button className="product-form-button">Ürün Ekle</button>
     </form>

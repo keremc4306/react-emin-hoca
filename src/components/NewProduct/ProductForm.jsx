46e8d2ee160608a7f8b9d169be9dfd1bd96b2b3b
Mehmet Kerem CANDANGIL <mkcandangil@sgk.gov.tr>
2025-08-13 09:40:53 +0300

    Form inputu dinleme

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -1,11 +1,15 @@
 import "./ProductForm.css";
 
 const ProductForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
     <form className="product-form">
       <div className="product-form-input">
         <label>Ürün Adı</label>
-        <input type="text" placeholder="Ürün Adı Giriniz..."/>
+        <input type="text" placeholder="Ürün Adı Giriniz..." onChange={titleChangeHandler}/>
       </div>
       <div className="product-form-input">
         <label>Ürün Fiyatı</label>
